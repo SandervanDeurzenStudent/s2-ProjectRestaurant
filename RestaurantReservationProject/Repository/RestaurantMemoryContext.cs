5bb8aca13c2c8440bc9d372804b8493792821754
Sander van Deurzen <D251669@edu.rocwb.nl>
2021-12-01 11:54:49 +0100

    changed names for Models(was exampleDto, now just example), read desc
    began with tesing with repositories

---
+++ RestaurantReservationProject/Repository/RestaurantMemoryContext.cs
@@ -0,0 +1,36 @@
+﻿using Repository.interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Repository
+{
+    public class RestaurantMemoryContext : IRestaurantContext
+    {
+        public void create(RestaurantDto restaurant)
+        {
+
+        }
+
+        public List<RestaurantDto> returnList()
+        {
+
+        }
+
+
+        public void Delete(int id)
+        {
+
+        }
+
+        public RestaurantDto getRestaurantById(int id)
+        {
+
+        }
+
+
+        public void update(int id, RestaurantDto restaurant)
+        {
+
+        }
+    }
+}
